49348b539a22f6080a74d204a49a587a8122df99
r.b4rr10s <a20202073@pucp.edu.pe>
2022-11-29 14:25:09 -0500

    Logueop

--- src/main/java/Daos/UsuarioDao.java
+++ src/main/java/Daos/UsuarioDao.java
@@ -1,6 +1,63 @@
 package Daos;
 
-public class UsuarioDao extends DaoBase{
+import Beans.TipoUsuario;
+import Beans.Usuario;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UsuarioDao extends DaoBase{
+    public Usuario loginUsuario(String username, String password){
+
+        Usuario usuario = new Usuario();
+
+        //Comparar primero con la tabla de usuarios
+        //antes del sql se debe hashear el password para comparar los hashes
+        String sql = "select * from Usuarios where correo=? and contrasenia=sha2(?,512)";
+        try (Connection connection = this.getConnection();
+             PreparedStatement pstmt = connection.prepareStatement(sql);) {
+            pstmt.setString(1, username);
+            pstmt.setString(2, password);
+            try (ResultSet rs = pstmt.executeQuery()) {
+                if (rs.next()) {
+                    int idUsuario = rs.getInt(1);
+                    usuario=buscarPorId(idUsuario);
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return usuario;
+    }
+
+    public Usuario buscarPorId(int idUsuario){
+        Usuario usuario = new Usuario();
+
+        String sql = "select u.idUsuarios, u.nombre, u.apellido, u.correo, tu.idTipoUsuario, tu.tipoUsuario\n" +
+                "from Usuarios u inner join TipoUsuario tu on u.idTipoUsuario=tu.idTipoUsuario where idUsuarios=?;";
+
+        try(Connection connection = this.getConnection();
+            PreparedStatement pstmt = connection.prepareStatement(sql)){
+            pstmt.setInt(1, idUsuario);
+            try(ResultSet rs = pstmt.executeQuery()){
+                if (rs.next()) {
+                    usuario.setIdUsuario(rs.getInt(1));
+                    usuario.setNombre(rs.getString(2));
+                    usuario.setApellido(rs.getString(3));
+                    usuario.setCorreo(rs.getString(4));
+                    TipoUsuario tu = new TipoUsuario();
+                    tu.setIdTipoUsuario(rs.getInt(5));
+                    tu.setTipoUsuario(rs.getString(6));
+                    usuario.setTipoUsuario(tu);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return usuario;
+    }
 
 }
--- src/main/java/Servlets/SessionServlet.java
+++ src/main/java/Servlets/SessionServlet.java
@@ -1,5 +1,7 @@
 package Servlets;
 
+import Beans.Usuario;
+import Daos.UsuarioDao;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
@@ -10,7 +12,20 @@ import java.io.IOException;
 public class SessionServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        HttpSession session = request.getSession();
+        String accion = request.getParameter("accion") == null ? "iniciar" : request.getParameter("accion");
+        RequestDispatcher view;
+        UsuarioDao uDao = new UsuarioDao();
+        switch (accion) {
+            case ("iniciar"):
+                HttpSession session1 = request.getSession();
+                if (session1.getAttribute("UsuLogueo") == null) {
+                    view = request.getRequestDispatcher("/Inicio/Login.jsp");
+                    view.forward(request, response);
+                } else {
+                    response.sendRedirect(request.getContextPath() + "/ServicioServlet");
+                }
+        }
 
 
 
@@ -19,7 +34,7 @@ public class SessionServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        EmpleadoDao empleadoDao = new EmpleadoDao();
+        UsuarioDao uDao = new UsuarioDao();
         HttpSession session = request.getSession();
 
 
@@ -33,31 +48,18 @@ public class SessionServlet extends HttpServlet {
                 String usuario = request.getParameter("usuario");
                 String password = request.getParameter("password");
 
-                Empleado empleado = empleadoDao.loginEmpleado(dni,password);
+                Usuario usuario1 = uDao.loginUsuario(usuario,password);
 
-                if(empleado!=null){
+                if(usuario1!=null){
                     //System.out.println("hola");
-                    session.setAttribute("EmpLogueo",empleado);
+                    session.setAttribute("UsuLogueo",usuario1);
                     //session.setMaxInactiveInterval(10*60);
-
-                    if(empleado.getRol().equals("gestor") || empleado.getRol().equals("vendedor")){
-                        response.sendRedirect(request.getContextPath() + "/CarteleraServlet");
-
-                    }else if (empleado.getRol().equals("admin")) {
-                        response.sendRedirect(request.getContextPath() + "/ReportesServlet");
-
-                    }
-
-
-
+                    response.sendRedirect(request.getContextPath() + "/ServicioServlet");
                 }else {
-
                     session.setAttribute("msg","La credenciales son incorrectas");
                     RequestDispatcher view = request.getRequestDispatcher("/Inicio/Login.jsp");
                     view.forward(request,response);
-
                 }
-
                 break;
 
             case ("registrar"):
@@ -68,8 +70,5 @@ public class SessionServlet extends HttpServlet {
                 break;
 
         }
-
-
-
     }
 }
